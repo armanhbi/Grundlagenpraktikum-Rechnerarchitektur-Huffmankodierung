960b5de81408dd3363265e1fd2275070a7a35fc7
Arman H <habibi.arman.2003@gmail.com>
2023-01-02 19:06:25 +0100

    added possible arguments and almost all error messages possible

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
+
+#include "huffman.c"
+#include "rahmenprogram.c"
 
 // Help message created with following standard: http://courses.cms.caltech.edu/cs11/material/general/usage.html
 const char *HELP_MSG = "\nusage: huffman <input_file> [-V version_num] [-B n] [-d] [-o output_file] [-h]\n\n"
-                       "\tinput_file: Path to input file used for Huffman en-/decoding\n\n"
-                       "\t-V <version_num>: Implementation version to use (as number, default: 0)\n"
-                       "\t-B <n>: Time measurement over n rounds (as number)\n"
-                       "\t-d: If set decrypting given Huffman code\n"
-                       "\t-o <output_file>: Output file to write result into\n"
-                       "\t-h: Shows this help menu\n\n";
+                       "\tinput_file:\t\tPath to input file used for Huffman en-/decoding\n\n"
+                       "\t-V <version_num>:\tImplementation version to use (as number, default: 0)\n"
+                       "\t-B <n>:\t\t\tTime measurement over n rounds (as number)\n"
+                       "\t-d:\t\t\tIf set decrypting given Huffman code\n"
+                       "\t-o <output_file>:\tOutput file to write result into\n"
+                       "\t-h:\t\t\tShows this help menu\n\n";
 
 /**
  * @brief Prints help message if arguments not valid (or -h was used)
@@ -17,7 +22,6 @@ void print_help() {
     fprintf(stderr, "%s", HELP_MSG);
 }
 
-// Main method executed with program start
 int main(int argc, char **argv) {
 
     // Checking if program and arguments valid
@@ -26,5 +30,52 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    return 0;
+    int impl_num = 0;
+    bool measure = false;
+    int measure_rounds = 0;
+    char* input_file;
+    bool decrypt = false;
+    char* output_file;
+
+    // --help for -h
+    static struct option help_synonym[] = {
+            {"help", no_argument, NULL, 'h'}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "V:B:o:dh", help_synonym, NULL)) != -1) {
+        switch(opt) {
+            case 'V':
+                impl_num = atoi(optarg);
+                break;
+            case 'B':
+                measure = true;
+                if (optarg == NULL) {
+                    print_help();
+                    exit(EXIT_FAILURE);
+                }
+                measure_rounds = atoi(optarg);
+                break;
+            case 'o':
+                output_file = optarg;
+                break;
+            case 'd':
+                decrypt = true;
+                break;
+            case 'h':
+            default:
+                print_help();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "%s", "Positional argument 'file' not found\n");
+        print_help();
+        return EXIT_FAILURE;
+    }
+
+    input_file = argv[optind];
+
+    return EXIT_SUCCESS;
 }
