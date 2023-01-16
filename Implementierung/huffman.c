666673c77bc0a4bd1108a7eba9b25ff4eb9fdfac
Arman H <arman.habibi@tum.de>
2023-01-16 17:14:51 +0100

    working simple huffman, beautify, etc.

--- Implementierung/huffman.c
+++ Implementierung/huffman.c
@@ -1,49 +1,49 @@
 #include "huffman.h"
 
-void encode_tree(struct node *tree, char *buffer, int *cur) {
+void encode_tree(struct node *tree, char *buffer, int *index) {
     if (tree->right == NULL && tree->left == NULL) {
         char character = tree->character;
-        buffer[(*cur)++] = '1';
+        buffer[(*index)++] = '1';
         printf("1 ");
         printf("%c (", character);
         for (int i = 9; i > 1; i--) {
             char c = (character & 0x80) >> 7 ? '1' : '0'; // Bit hack to get the first bit of the character
-            buffer[(*cur)++] = c;
+            buffer[(*index)++] = c;
             character <<= 1;
             putchar(c);
         }
         printf(") ");
         return;
     }
-    buffer[(*cur)++] = '0';
+    buffer[(*index)++] = '0';
     printf("0 ");
-    encode_tree(tree->left, buffer, cur);
-    encode_tree(tree->right, buffer, cur);
+    encode_tree(tree->left, buffer, index);
+    encode_tree(tree->right, buffer, index);
 }
 
-struct node *decode_tree(char *compressed, int *cur) {
+struct node *decode_tree(char *compressed, int *index) {
     struct node *cur_node = create_node('\0', 0);
     cur_node->left = NULL;
     cur_node->right = NULL;
 
-    while (compressed[*cur] != '\0') {
-        if (compressed[*cur] == 49) { // ascii for '1'
+    while (compressed[*index] != '\0') {
+        if (compressed[*index] == 49) { // ascii for '1'
             char character;
             for (int i = 1; i < 9; i++) {
                 character <<= 1;
-                if (compressed[(*cur) + i] - 49 == 0) {
+                if (compressed[(*index) + i] - 49 == 0) {
                     character++;
                 }
             }
             cur_node->character = character;
-            (*cur) += 8;
+            (*index) += 8;
             return cur_node;
         }
-        ++(*cur);
-        cur_node->left = decode_tree(compressed, cur);
-        ++(*cur);
-        cur_node->right = decode_tree(compressed, cur);
-        (*cur)++;
+        ++(*index);
+        cur_node->left = decode_tree(compressed, index);
+        ++(*index);
+        cur_node->right = decode_tree(compressed, index);
+        (*index)++;
     }
     return cur_node;
 }
@@ -55,13 +55,10 @@ struct node *decode_tree(char *compressed, int *cur) {
  * @return Returns a string consisting of binary code AND tree
  */
 char *huffman_encode(size_t len, const char data[len]) {
+    int table[128] = {0}; // create table (same as ascii table)
 
-    // create table (same as ascii table)
-    int table[128] = {0};
-
-    // increment for every letter
     for (int i = 0; i < len; i++) {
-        table[(int) data[i]] += 1;
+        table[(int) data[i]] += 1; // increment for every letter (counting appearance)
     }
 
     // print (for debugging)
@@ -73,8 +70,7 @@ char *huffman_encode(size_t len, const char data[len]) {
     }
     printf("\n");
 
-    // create tree
-    struct node *root = NULL;
+    struct node *root = NULL; // create tree
 
     // Fill table with values (search minIndex and use it and set it to 0)
     while (1) {
@@ -107,7 +103,6 @@ char *huffman_encode(size_t len, const char data[len]) {
     uint8_t length_table[128] = {0};
     uint16_t lookup_table[128] = {0};
 
-    int cur[1] = {0};
     tree_to_dic(root, length_table, lookup_table, 0, 0);
 
     // print (for debugging)
@@ -122,16 +117,19 @@ char *huffman_encode(size_t len, const char data[len]) {
     }
     printf("\n");
 
-    char *huffman = malloc(65536 * sizeof(char));
+    char huffman[BUF_LENGTH] = {0};
 
     int huffman_index = 0;
     for (int i = 0; i < len; i++) {
         uint16_t code = lookup_table[data[i]];
-        uint16_t mask = 0x8000;
+        uint8_t length = length_table[data[i]];
+        uint16_t mask = 1 << (length - 1);
 
-        while (code) {
-            huffman[huffman_index++] = (code & mask) ? '1' : '0';
-            code<<=1;
+        int iteration = 0;
+        while (mask) {
+            huffman[huffman_index++] = ((code & mask) >> (length - 1 - iteration)) ? '1' : '0';
+            mask>>=1;
+            iteration++;
         }
     }
 
@@ -139,12 +137,12 @@ char *huffman_encode(size_t len, const char data[len]) {
     for (int i = 0; i < huffman_index; i++) {
         printf("%c", huffman[i]);
     }
-    printf("%s\n", WHITE);
+    printf("%s\n\n", WHITE);
 
     char *buffer = malloc(512 * sizeof(char)); // malloc check
-    *cur = 0;
+    int index[1] = {0};
     printf("%sCompressed tree%s\n", CYAN, WHITE); // print (for debugging)
-    encode_tree(root, buffer, cur);
+    encode_tree(root, buffer, index);
 
     return buffer;
 }
--- Implementierung/huffman.h
+++ Implementierung/huffman.h
@@ -8,8 +8,8 @@
 #include "tree.h"
 #include "input_output.h"
 
-void encode_tree(struct node *tree, char *buffer, int *cur);
-struct node *decode_tree(char *compressed, int *cur);
+void encode_tree(struct node *tree, char *buffer, int *index);
+struct node *decode_tree(char *compressed, int *index);
 char *huffman_encode(size_t len, const char data[len]);
 char *huffman_decode(size_t len, const char data[len]);
 
